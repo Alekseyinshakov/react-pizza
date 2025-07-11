c24637382871922beaf1ea26fa61842e87930f78
Alexey Inshakov <alexeyinshakov992@gmail.com>
2025-07-11 13:38:49 +0300

    feat: add SCSS support, and clear project

---
+++ src/components/colorBox/colorBoxComponent.tsx
@@ -0,0 +1,11 @@
+import s from './colorBoxComponent.module.scss'
+
+function ColorBoxComponent() {
+  return (
+    <div className={s.main}>
+      Square
+    </div>
+  );
+}
+
+export default ColorBoxComponent;
