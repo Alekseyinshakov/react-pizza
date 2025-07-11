ef53fad2e35d344a1ce87f99b75e33dd94e997c9
Alexey Inshakov <alexeyinshakov992@gmail.com>
2025-07-11 14:27:05 +0300

    feat: change title

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,8 @@
-import s from './App.module.scss'
 
 function App() {
   return (
     <>
-      Hello world
-      <ul className={s.list}>
-        <li>qwerty</li>
-        <li>qwerty</li>
-        <li>qwerty</li>
-        <li>qwerty</li>
-        <li>qwerty</li>
-        <li>qwerty</li>
-        <li>qwerty</li>
-        <li>qwerty</li>
-      </ul>
+
     </>
   )
 }
