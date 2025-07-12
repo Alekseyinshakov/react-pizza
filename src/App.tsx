6889eaa70b0676e6b367bc72bc17f548f7983112
Alexey <alexeyinshakov992@gmail.com>
2025-07-12 17:29:34 +0300

    feat: add and setup prettier

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,6 @@
-
 function App() {
-  return (
-    <>
-
-      project ready for developing 95
-
-    </>
-  )
+  const abra = { a: 1, b: 2, c: 3 };
+  return <>project ready for developing 95</>;
 }
 
-export default App
+export default App;
