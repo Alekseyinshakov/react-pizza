4c074003b82a4944f5aba18b9c42a5d15c5f031c
Alexey Inshakov <alexeyinshakov992@gmail.com>
2025-08-23 14:56:56 +0300

    fix: fix url in detailed page component

--- src/pages/DetailedPage.tsx
+++ src/pages/DetailedPage.tsx
@@ -72,7 +72,7 @@ const DetailedPage = () => {
     const getPizza = async () => {
       try {
         const response = await axios.get(
-          `https://react-pizza-api-one.vercel.app/${id}`
+          `https://react-pizza-api-one.vercel.app/products/${id}`
         )
         setPizzaData(response.data)
         setStatus('success')
